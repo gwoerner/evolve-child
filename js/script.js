b8de9a66e12a807b635408479568b435bb030015
Geraldine <geraldine@cyghes.fr>
2017-11-14 09:17:18 +0100

    display contents

--- js/script.js
+++ js/script.js
@@ -15,7 +15,5 @@ window.onscroll = function () {
 function scrollDownFunction() {
     if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
         document.getElementById("logo-image").style.display = "contents";
-    } else {
-        document.getElementById("logo-image").style.display = "none";
-    }
+    } 
 }
