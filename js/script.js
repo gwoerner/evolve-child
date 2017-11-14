a35249ddf4d75dfaa35cf5cdc838fe906c5baaeb
Geraldine <geraldine@cyghes.fr>
2017-11-14 09:12:39 +0100

    affichage du logo-image en float

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ window.onscroll = function () {
 
 function scrollDownFunction() {
     if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-        document.getElementById("logo-image").style.display = "block";
+        document.getElementById("logo-image").style.float = "left";
     } else {
         document.getElementById("logo-image").style.display = "none";
     }
