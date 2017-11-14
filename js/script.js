1196be61eb61ae0e4b85e7221dd53b3f0ae954a4
Geraldine <geraldine@cyghes.fr>
2017-11-14 09:00:56 +0100

    ajout fichier js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+//  When the user scrolls down 40px from the top of the document, show the logo in the header
+
+window.onscroll = function () {
+    scrollDownFunction();
+//    scrollStart();
+};
+
+function scrollDownFunction() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        document.getElementById("logo-image").style.display = "block";
+    } else {
+        document.getElementById("logo-image").style.display = "none";
+    }
+}
